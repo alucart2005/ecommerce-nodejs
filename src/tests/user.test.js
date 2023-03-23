cea400107ab92b1113a87a1f83812127bad39c7d
Napoleon Anaya Romero <113813382+alucart2005@users.noreply.github.com>
2023-03-23 14:09:37 -0500

    start

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -4,7 +4,7 @@ const app = require('../app');
 let userId;
 let token;
 
-test('POST /users', async () => {
+test('POST /users should create a user', async () => {
   const newUser = {
     "firstName": "Nombre1",
     "lastName": "Anaya1",
@@ -20,7 +20,33 @@ test('POST /users', async () => {
   expect(res.body.email).toBe(newUser.email);
 });
 
-test('GET /users', async () => {
+test('POST /users/login should de login', async () => {
+  const user = {
+    "email": "nombre@emial.com",
+    "password": "1234",
+  };
+  const res = await request(app)
+    .post('/users/login')
+    .send(user);
+  token = res.body.token;  
+  expect(res.statusCode).toBe(200);
+  expect(res.body.user.email).toBe(user.email);
+  expect(res.body.token).toBeDefined();
+
+});
+
+test('POST /users/login with invalid credentials should return 401', async () => {
+  const user = {
+    "email": "nombre@emial.com",
+    "password": "12345",
+  };
+  const res = await request(app)
+    .post('/users/login')
+    .send(user);
+  expect(res.status).toBe(401);
+});
+
+test('GET /users  should return all users', async () => {
   const res = await request(app)
   .get('/users')
   .set('Authorization', `Bearer ${token}`);
@@ -28,7 +54,7 @@ test('GET /users', async () => {
   expect(res.body).toHaveLength(1);
 });
 
-test('PUT /users/:id', async () => {
+test('PUT /users/:id should update one user', async () => {
   const newUser = {
     "firstName": "Nombre1",
     "lastName": "Anaya1",
@@ -38,40 +64,16 @@ test('PUT /users/:id', async () => {
   };
   const res = await request(app)
     .put(`/users/${userId}`)
-    .send(newUser);
+    .send(newUser)
+    .set('Authorization', `Bearer ${token}`);
   expect(res.statusCode).toEqual(200);
   expect(res.body.firstName).toBe(newUser.firstName);
   expect(res.body.lastName).toBe(newUser.lastName);
 });
 
-test('LOGIN /users/login', async () => {
-  const user = {
-    "email": "nombre@emial.com",
-    "password": "1234",
-  };
+test('DELETE /users/:id should delete one user', async () => {
   const res = await request(app)
-    .post('/users/login')
-    .send(user);
-  expect(res.statusCode).toBe(200);
-  expect(res.body.user.email).toBe(user.email);
-  expect(res.body.token).toBeDefined();
-
-});
-
-test('POST /users/login', async () => {
-  const user = {
-    "email": "nombre@emial.com",
-    "password": "12345",
-  };
-  const res = await request(app)
-    .post('/users/login')
-    .send(user);
-  expect(res.status).toBe(401);
-
-});
-
-test('DELETE /users/:id', async () => {
-  const res = await request(app).delete(`/users/${userId}`);
+  .delete(`/users/${userId}`)
+  .set('Authorization', `Bearer ${token}`);
   expect(res.statusCode).toBe(204);
 });
-

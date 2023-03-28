a0c43d3398688fc4c8b21d66a98c852813f014a1
Napoleon Anaya Romero <113813382+alucart2005@users.noreply.github.com>
2023-03-27 21:48:58 -0500

    .

--- src/controllers/category.controllers.js
+++ src/controllers/category.controllers.js
@@ -1,33 +1,33 @@
 const catchError = require('../utils/catchError');
-const category = require('../models/category');
+const Category = require('../models/Category  ');
 const Product = require('../models/Product');
 
 const getAll = catchError(async(req, res) => {
-    const results = await category.findAll({ include: Product });
+    const results = await Category.findAll({ include: Product });
     return res.json(results);
 });
 
 const create = catchError(async(req, res) => {
-    const result = await category.create(req.body);
+    const result = await Category .create(req.body);
     return res.status(201).json(result);
 });
 
 const getOne = catchError(async(req, res) => {
     const { id } = req.params;
-    const result = await category.findByPk(id);
+    const result = await Category .findByPk(id);
     if(!result) return res.sendStatus(404);
     return res.json(result);
 });
 
 const remove = catchError(async(req, res) => {
     const { id } = req.params;
-    await category.destroy({ where: {id} });
+    await Category.destroy({ where: {id} });
     return res.sendStatus(204);
 });
 
 const update = catchError(async(req, res) => {
     const { id } = req.params;
-    const result = await category.update(
+    const result = await Category .update(
         req.body,
         { where: {id}, returning: true }
     );
